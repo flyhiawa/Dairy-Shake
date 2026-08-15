123f4fd67050f640cb60fe70b7e4fccf4fb51930
flyhiawa <flyhiawa@gmail.com>
2026-08-15 08:38:16 +0000

    remove target

--- Source/target/debug/deps/dysk-acf8e2858aaa7485.d
+++
@@ -1,9 +0,0 @@
-/home/flyhiawa/Projetos/Dairy-Shake/Source/target/debug/deps/dysk-acf8e2858aaa7485.d: src/main.rs src/types.rs src/lexer.rs src/parser.rs src/result.rs
-
-/home/flyhiawa/Projetos/Dairy-Shake/Source/target/debug/deps/libdysk-acf8e2858aaa7485.rmeta: src/main.rs src/types.rs src/lexer.rs src/parser.rs src/result.rs
-
-src/main.rs:
-src/types.rs:
-src/lexer.rs:
-src/parser.rs:
-src/result.rs:
--- Source/target/debug/deps/proj-ec9481e48a123622.d
+++
@@ -1,5 +0,0 @@
-/home/flyhiawa/Maltihl/proj/target/debug/deps/proj-ec9481e48a123622.d: src/main.rs
-
-/home/flyhiawa/Maltihl/proj/target/debug/deps/libproj-ec9481e48a123622.rmeta: src/main.rs
-
-src/main.rs:
